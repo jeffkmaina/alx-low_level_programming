ca839a50e528b0e8ffbe95f73aa202c1953eb987
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-10 14:46:26 +0000

    Task 4 puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -prints the phrase Programming is like building a multilingual puzzle
- * Return 0 if exited ok, non ero otherwise
+ * main - prints the phrase Programming is like building a multilingual puzzle
+ * Return - 0 if exited ok, non zero otherwise
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts ("\"Programming is like building a multilingual puzzle\n");
 
 	return(0);
 }
