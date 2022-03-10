c75183e58701c980dc48252e9c50412e680ffc8f
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-10 16:00:22 +0000

    Various sies of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/** main - prints various types' sizes on computer
+ * Return: 0 successfully exited non zero Otherwise
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", 1);
+	printf("Size of an int: %d byte(s)\n", 4);
+	printf("Size of a long int: %d byte(s)\n", 4);
+	printf("Size of a long long int: %d byte(s)\n", 8);
+	printf("Size of a float: %d byte(s)\n", 4);
+	return (0);
+}
