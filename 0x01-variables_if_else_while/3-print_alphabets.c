dc874fe9e193f8fc928ad5c5be3ad32caa16617d
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-11 14:30:46 +0000

    Added task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -Entry Point
+ * Return: 0 exited successfully
+ */
+
+int main(void)
+{
+	char ch;
+	char CH;
+
+	while (ch <= 'z')
+		putchar(ch);
+		ch++;
+	while (ch <= 'Z')
+		putchar(CH);
+		CH++;
+	putchar('\n');
+	return (0);
+}
