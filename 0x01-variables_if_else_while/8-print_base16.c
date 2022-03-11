f07b8f776404577d397967d7e36cf558fd6f1518
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-11 20:14:59 +0000

    Added Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print all numbers base 16
+ * Return: 0 Exits successfully
+ */
+
+int main(void)
+{
+	char a = '0';
+	char b = 'a';
+
+	while (a <= '8')
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 'f')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
