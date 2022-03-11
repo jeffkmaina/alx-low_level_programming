eeacc6bc5f68cde39cff5a776b44ce62bb286405
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-11 13:46:09 +0000

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 if exited successfully non zero OW
+**/
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar (x);
+	return (0);
+}
