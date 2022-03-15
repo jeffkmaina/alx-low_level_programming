4b95ce70dddc3135ee1fb548247ca2dea3952148
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-15 20:31:33 +0000

    Added task 4

---
+++ 0x02-functions_nested_loops/4-isaplha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic
+ * character
+ * @c: checks character
+ * Return: 1 if letter 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <='Z'));
+}
