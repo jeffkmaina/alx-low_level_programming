d679acd919075660af2421c91ac8736c3ccb9b24
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-15 21:13:30 +0000

    Added task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last
+ * integer of number
+ * @n: check integer
+ * Return: return last value of digit
+ */
+
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+	{
+		n = -n
+	}
+	a = n % 10;
+
+	if (a < 0)
+	{
+		a = -a;
+	}
+	_putchar(a + '0');
+	return(a);
+}
