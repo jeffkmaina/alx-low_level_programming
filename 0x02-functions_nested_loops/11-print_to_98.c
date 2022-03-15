a0b7d870ad4c566858f41dcbf30f2f5188bf1ba5
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-15 22:30:07 +0000

    Added task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural
+ * numbers from n to 98
+ * @n: beginning of int
+ */
+
+void print_to_98(int n)
+{
+	int sum;
+
+	sum = 0;
+	n = 0;
+
+	while (n < 1024)
+	{
+		if (n % 3 == 0) || (n % 5 == 0))
+		{
+			sum += n;
+		}
+	n++;
+	}
+}	
