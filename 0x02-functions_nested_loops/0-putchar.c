583b5032ebf384e088ff59385260411246a34c70
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-15 11:23:11 +0000

    Added task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * main - prints __putchar
+ * Return: 0 exits successfully
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}	
+	
