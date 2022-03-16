da5c7ff51d84fa75b40ed5462caa1924164b0a18
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-16 14:36:00 +0000

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * * main - causes an infinite loop
- * * Return: 0
- * */
+ * main - causes an infinite loop
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -16,6 +16,9 @@ int main(void)
  *	{
  *		putchar(i);
  *	}
- */     printf("Infinite loop avoided! \\o/\n");
+ */     
+	
+	printf("Infinite loop avoided! \\o/\n");
+	
 	return (0);
 }
