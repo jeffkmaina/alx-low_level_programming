a16731bf727b53cb37166ea60797865984006dd6
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-17 12:15:59 +0000

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 to
+ * 9 followed by a new line
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	c = '0'
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+}
