e0fb4944f6549d0022a4151bd82cf1fb4fff3715
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-17 13:14:11 +0000

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,14 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 10; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b <= 14; b++)
+		for (b = 0; b < 15; b++)
 		{
 			if (b >= 10)
-			{
+			
 				_putchar(b / 10 + '0');
-				_putchar(b % 10 + '0');
-			}
+			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
 	}
