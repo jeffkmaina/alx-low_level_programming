ba9e2a85c89aa77efebe695e9fcb8b87d854814a
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-28 20:24:01 +0000

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,7 +2,7 @@
 
 /**
  * _strchr - function that locates character in a string
- * @s: string 
+ * @s: string
  * @c: character to be located
  * Return: pointer to first occurrence of c
  * NULL if the characer is not found
