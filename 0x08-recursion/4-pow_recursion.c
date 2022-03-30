89fa93911441dab38a26008126bfe18b3990c5c1
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-30 18:21:33 +0000

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,12 +11,12 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return(-1);
+		return (-1);
 	if (y == 0)
 		return (1);
 	if (y == 1)
 		return (x);
 	if (y % 2 == 0)
-		return _pow_recursion(x * x, y / 2);
-	return x*_pow_recursion(x * x, (y - 1) / 2);
+		return (_pow_recursion(x * x, y / 2));
+	return (x * _pow_recursion(x * x, (y - 1) / 2));
 }
