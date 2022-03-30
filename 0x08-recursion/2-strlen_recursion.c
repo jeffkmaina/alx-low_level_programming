8c147690d27296bd8d1413ded61f46fa1bb33709
Jeffrey Maina <jeffrey.k.maina@gmail.com>
2022-03-30 11:45:08 +0000

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that prints length of a string
+ * @s: string to be evaluated
+ * Return: eturns length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	unsigned int i = 0;
+
+	if (s[i] == '\0')
+	{
+		return 0;
+	}
+	else
+		return 1 + _strlen_recursion(s + 1);
+}
