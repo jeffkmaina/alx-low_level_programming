544f051d32ccadada45ca4d6b38cf71e545840bd
jeffkmaina <jeffrey.k.maina@gmail.com>
2022-04-07 18:02:53 +0000

    task 5

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,3 +4,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
