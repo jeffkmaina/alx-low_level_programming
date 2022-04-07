2964d56b165acf1e9776e4746d5621393c14577a
jeffkmaina <jeffrey.k.maina@gmail.com>
2022-04-07 13:06:41 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,9 +37,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (n < len2 && i < (len1 + n))
 		s3[i++] = s2[j++];
 
-	while (n >= len2 && i < (len1 +len2))
-			s3[i++] = s2[j++];
-	
+	while (n >= len2 && i < (len1 + len2))
+		s3[i++] = s2[j++];
+
 	s3[i++] = '\0';
 
 	return (s3);
