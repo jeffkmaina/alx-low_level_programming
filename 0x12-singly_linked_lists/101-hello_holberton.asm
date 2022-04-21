1b1ef2b4c481816b4bccc72f33ed222d09d27329
jeffkmaina <jeffrey.k.maina@gmail.com>
2022-04-21 15:04:56 +0000

    Task 6 correction

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,21 @@
+extern printf
 
-bal    main
-          extern    printf
+section .text
+global main
+
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
+push rbp
 
+mov rdi, fmt
+mov rsi, msg
+mov rax, 0
+call printf
+
+pop rbp
+
+mov rax, 0
+ret
+
+section .data
+msg: db "Hello, Holberton", 0
+fmt: db "%s", 10, 0
