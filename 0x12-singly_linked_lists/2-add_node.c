b5014300b5f42fcff87e95e41988fa126de6d403
jeffkmaina <jeffrey.k.maina@gmail.com>
2022-04-21 14:33:12 +0000

    task 2 betty

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,15 +4,16 @@
 #include <stdlib.h>
 
 /**
- *  * _strlen - find string length
- *   * @str: string
- *    * Return: length
- *     */
+ * _strlen - find string length
+ * @str: string
+ * Return: length
+ */
 int _strlen(const char *str)
 {
 	int len;
 
-	for (len = 0; str[len] != '\0'; len++);
+	for (len = 0; str[len] != '\0'; len++)
+		;
 	return (len);
 }
 /**
