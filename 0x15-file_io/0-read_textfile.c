fff03cd4beb011525d9882a31dc5bc132398e02d
jeffkmaina <jeffrey.k.maina@gmail.com>
2022-05-03 18:18:06 +0000

    fixed betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,12 +19,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	
+
 	if (fd < 0)
 		return (0);
 
 	buf = malloc(sizeof(char) * letters);
-	
+
 	if (buf == NULL)
 		return (0);
 
