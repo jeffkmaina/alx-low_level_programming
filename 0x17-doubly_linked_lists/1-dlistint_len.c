8bef43772d1e8e90168b38aa87049d381aeab410
jeffkmaina <jeffrey.k.maina@gmail.com>
2022-06-09 11:41:53 +0000

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * size_t dlistint_len - function that returns the number of elements
+ * in a llinked list
+ * @h: pointer to node
+ * Return: number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nodes += 1;
+
+	}
+	return (nodes);
+}
